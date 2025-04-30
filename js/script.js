78334680b41aa6142a4e9983fdb88e38bc5c5f61
KMStanvir1998 <islam.tanvir.2015bd@gmail.com>
2025-04-30 13:14:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+ var elem = document.querySelector('.main-carousel');
+  var flickity = new Flickity( elem, {
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,    
+    autoPlay: 3000,      
+    pauseAutoPlayOnHover: true
+  });
+
+  
